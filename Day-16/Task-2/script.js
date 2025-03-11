a1c5ce914c3c7af78c1c7811281770db2d51e6e9
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2025-03-11 10:55:29 +0530

    added styling for tasks day-16

--- Day-16/Task-2/script.js
+++ Day-16/Task-2/script.js
@@ -1,6 +1,7 @@
 let passwordTypes = ["Strong", "Medium", "Weak"];
 
 let input1 = document.getElementById("password");
+
 let match = document.getElementById("match");
 
 let btn = document.getElementsByTagName("button")[0];
@@ -16,11 +17,15 @@ if (input1 && match && btn) {
 
     if (password.length <= 3) {
       match.innerText = passwordTypes[2];
+      match.style.color = "red";
     } else if (password.length <= 5) {
       match.innerText = passwordTypes[1];
+      match.style.color = "orange";
     } else {
       match.innerText = passwordTypes[0];
+      match.style.color = "green";
     }
+    document.getElementById("password").value = "";
   });
 } else {
   console.error("Password input or match element not found.");
